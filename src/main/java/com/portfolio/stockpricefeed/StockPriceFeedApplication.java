dbb5dd5fbe408a19537967d56d33d25ebc1402fb
BL-Vignesh <vignesh.raju@bridgelabz.com>
2026-04-12 11:54:32 +0530

    Add files for Use Case 6

---
+++ src/main/java/com/portfolio/stockpricefeed/StockPriceFeedApplication.java
@@ -0,0 +1,20 @@
+package com.portfolio.stockpricefeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class StockPriceFeedApplication {
+
+	static {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceFeedApplication.class, args);
+	}
+
+}
